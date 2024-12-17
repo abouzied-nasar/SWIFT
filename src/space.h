b69ff10d2bfcdcc694e7f337cdf4234ec7b37366
Abouzied <abouzied.nasar@gmail.com>
2024-12-17 13:49:28 +0000

    Added more debug checks to figure out why we got cells with 8x expected particles. Also added a modified scheduler_splittasks() function to split GPU tasks but still no joy. Could it be that some cells are not given progeny with size 2h^3?
    Hard-coded space_splitsize_default to 100 with no avail. Something fishy is going on...
    
    Hard-coded space_grid_split_threshold_default to 100 and space_subsize_pair_hydro_default with no avail. Something fishy is going on...

--- src/space.h
+++ src/space.h
@@ -46,9 +46,9 @@ struct hydro_props;
 
 /* Some constants. */
 #define space_cellallocchunk 1000
-#define space_splitsize_default 400
+#define space_splitsize_default 100
 #define space_maxsize_default 8000000
-#define space_grid_split_threshold_default 400
+#define space_grid_split_threshold_default 100
 #define space_extra_parts_default 0
 #define space_extra_gparts_default 200
 #define space_extra_sparts_default 200
