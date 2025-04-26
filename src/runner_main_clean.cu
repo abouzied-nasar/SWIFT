9811f3b36070f3c252daf8fdff8d9f232cd461d7
Abouzied <abouzied.nasar@gmail.com>
2025-04-26 10:56:03 +0100

    Not running ghost tasks shows that the difference is indeed in the near periodic particles and how they are treated! Subtracting recursed results from unrecursed results for zeroth step shows this clearly

--- src/runner_main_clean.cu
+++ src/runner_main_clean.cu
@@ -1020,16 +1020,6 @@ void *runner_main2(void *data) {
                       parts_aos_pair_f4_send, e, fparti_fpartj_lparti_lpartj_dens, &n_leaves_found, depth, n_expected_tasks,
 					  cells_left, cells_right);
 
-            for(int i = 0; i < n_leaves_found; i++){
-            	if(pack_vars_pair_dens->leaf_list[top_tasks_packed].ci[i]->hydro.count == 0 ||
-            			pack_vars_pair_dens->leaf_list[top_tasks_packed].cj[i]->hydro.count == 0)
-            		error("Something's not right");
-//            	struct cell * ci_tmp = pack_vars_pair_dens->leaf_list[top_tasks_packed].ci[i];
-//            	struct cell * cj_tmp = pack_vars_pair_dens->leaf_list[top_tasks_packed].cj[i];
-//            	message("Positions xi %f yi %f zi %f counti %i xj %f yj %f zj %f countj %i",
-//            			ci_tmp->loc[0], ci_tmp->loc[1], ci_tmp->loc[2], ci_tmp->hydro.count,
-//            			cj_tmp->loc[0], cj_tmp->loc[1], cj_tmp->loc[2], cj_tmp->hydro.count);
-            }
             message("Found %i daughter tasks", n_leaves_found);
 
             n_leafs_total += n_leaves_found;
@@ -1430,7 +1420,7 @@ void *runner_main2(void *data) {
           runner_do_init_grav(r, ci, 1);
           break;
         case task_type_ghost:
-          runner_do_ghost(r, ci, 1);
+//          runner_do_ghost(r, ci, 1);
           break;
 #ifdef EXTRA_HYDRO_LOOP
         case task_type_extra_ghost:
