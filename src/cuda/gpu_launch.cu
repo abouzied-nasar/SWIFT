87fa14c45d31a90e7377dc22cd8ead47ccc3e1f3
Abouzied <abouzied.nasar@gmail.com>
2026-01-28 13:46:25 +0000

    Found a bug when running on laptop. Somehow some cell end indices are smaller than zero e.g.:ci_end 0 cj_end 214, ci_end 712 cj_end 0, ci_end 0 cj_end 1210, ci_end 0 cj_end 1698. Unsure why this was not caught by workstation compiler

--- src/cuda/gpu_launch.cu
+++ src/cuda/gpu_launch.cu
@@ -63,77 +63,84 @@ __global__ void cuda_launch_density(
   const int threadid = blockDim.x * blockIdx.x + threadIdx.x;
   const int cid = threadid;
 
-  /* First, grab handles for where cells start and end */
-  int4 cell_starts_ends_read = d_cell_i_j_start_end[cid];
-  int4 cell_starts_ends_write = d_cell_i_j_start_end_non_compact[cid];
-  /*Assign without accounting for ci/cj_end being the index of cell positions.
-   * This will be accounted for in cuda_kernel_density()*/
-  const int ci_start = cell_starts_ends_read.x;
-  const int ci_end = cell_starts_ends_read.y;
-  const int cj_start = cell_starts_ends_read.z;
-  const int cj_end = cell_starts_ends_read.w;
+  /*Necessary to prevent out of bounds access
+   * for thread ids above what is necessary to
+   * complete computations*/
+  if(cid < bundle_n_cells){
+    /* First, grab handles for where cells start and end */
+    int4 cell_starts_ends_read = d_cell_i_j_start_end[cid];
+    int4 cell_starts_ends_write = d_cell_i_j_start_end_non_compact[cid];
+    /*Assign without accounting for ci/cj_end being the index of cell positions.
+     * This will be accounted for in cuda_kernel_density()*/
+    const int ci_start = cell_starts_ends_read.x;
+    const int ci_end = cell_starts_ends_read.y;
+    const int cj_start = cell_starts_ends_read.z;
+    const int cj_end = cell_starts_ends_read.w;
 
-  const int ci_write_start = cell_starts_ends_write.x;
-  const int ci_write_end = cell_starts_ends_write.y;
-  const int cj_write_start = cell_starts_ends_write.z;
-  const int cj_write_end = cell_starts_ends_write.w;
+//    const int ci_write_start = cell_starts_ends_write.x;
+//    const int ci_write_end = cell_starts_ends_write.y;
+    const int cj_write_start = cell_starts_ends_write.z;
+    const int cj_write_end = cell_starts_ends_write.w;
 
-  /*First things first. Find the shifts in case we're periodic*/
-  /*Step I: Get the cell positions*/
-  const struct gpu_cell_pos_d ci_loc = d_parts_send[ci_end - 1].c_loc;
-  const struct gpu_cell_pos_d cj_loc = d_parts_send[cj_end - 1].c_loc;
+    /*First things first. Find the shifts in case we're periodic*/
+    /*Step I: Get the cell positions*/
+    if(ci_end <= 0 || cj_end <= 0){
+      printf("indices smaller than zero ci_end %i cj_end %i\n", ci_end, cj_end);
+    }
+    const struct gpu_cell_pos_d ci_loc = d_parts_send[ci_end - 1].c_loc;
+    const struct gpu_cell_pos_d cj_loc = d_parts_send[cj_end - 1].c_loc;
 
-  double3 shift = {0.0, 0.0, 0.0};
+    double3 shift = {0.0, 0.0, 0.0};
 
-  const double distx = cj_loc.x.x - ci_loc.x.x;
-  const double disty = cj_loc.x.y - ci_loc.x.y;
-  const double distz = cj_loc.x.z - ci_loc.x.z;
+    const double distx = cj_loc.x.x - ci_loc.x.x;
+    const double disty = cj_loc.x.y - ci_loc.x.y;
+    const double distz = cj_loc.x.z - ci_loc.x.z;
 
-  /*Fine for now as thread divergence
-   * will be one (three) line of code*/
-  if(distx < -space_dim.x * 0.5)
-    shift.x = space_dim.x;
-  else if(distx > space_dim.x * 0.5)
-    shift.x = -space_dim.x;
+    /*Fine for now as thread divergence
+     * will be one (three) line of code*/
+    if(distx < -space_dim.x * 0.5)
+      shift.x = space_dim.x;
+    else if(distx > space_dim.x * 0.5)
+      shift.x = -space_dim.x;
 
-  if(disty < -space_dim.y * 0.5)
-    shift.y = space_dim.y;
-  else if(disty > space_dim.y * 0.5)
-    shift.y = -space_dim.y;
+    if(disty < -space_dim.y * 0.5)
+      shift.y = space_dim.y;
+    else if(disty > space_dim.y * 0.5)
+      shift.y = -space_dim.y;
 
-  if(distz < -space_dim.z * 0.5)
-    shift.z = space_dim.z;
-  else if (distz > space_dim.z * 0.5)
-    shift.z = -space_dim.z;
+    if(distz < -space_dim.z * 0.5)
+      shift.z = space_dim.z;
+    else if (distz > space_dim.z * 0.5)
+      shift.z = -space_dim.z;
 
-  const double cell_dist = sqrt(distx*distx + disty*disty + distz*distz);
+    const double cell_dist = sqrt(distx*distx + disty*disty + distz*distz);
 
-  /*Note: Since shift would be zero for self tasks where ci==cj
-   * this is fine as is to avoid divergence*/
-  const double shift_ix = shift.x + cj_loc.x.x;
-  const double shift_iy = shift.y + cj_loc.x.y;
-  const double shift_iz = shift.z + cj_loc.x.z;
+    /*Note: Since shift would be zero for self tasks where ci==cj
+     * this is fine as is to avoid divergence*/
+    const double shift_ix = shift.x + cj_loc.x.x;
+    const double shift_iy = shift.y + cj_loc.x.y;
+    const double shift_iz = shift.z + cj_loc.x.z;
 
 
-//  const double shift_ix = ci_loc.x.x;
-//  const double shift_iy = ci_loc.x.y;
-//  const double shift_iz = ci_loc.x.z;
+    //  const double shift_ix = ci_loc.x.x;
+    //  const double shift_iy = ci_loc.x.y;
+    //  const double shift_iz = ci_loc.x.z;
 
-  const double3 shift_i_res = {shift_ix, shift_iy, shift_iz};
-  const double3 shift_j_res = {cj_loc.x.x, cj_loc.x.y, cj_loc.x.z};
-//  if (cid < bundle_n_cells) {
-	/*Interact ci with cj*/
-//    printf("Doing ci\n");
+    const double3 shift_i_res = {shift_ix, shift_iy, shift_iz};
+    const double3 shift_j_res = {cj_loc.x.x, cj_loc.x.y, cj_loc.x.z};
+    //  if (cid < bundle_n_cells) {
+    /*Interact ci with cj*/
+    //    printf("Doing ci\n");
     cuda_kernel_density(cid, d_parts_send, d_parts_recv, d_a, d_H,
-    		cell_starts_ends_read, cell_starts_ends_write,
-    		space_dim, shift_i_res, shift_j_res);
+        cell_starts_ends_read, cell_starts_ends_write,
+        space_dim, shift_i_res, shift_j_res);
     /*Check if this is a self interaction.
      * If it is, skip as we don't need to re-do computations
      * We could just let threads do this again to avoid
      * divergence since we only unpack ci once on host
      * (for it's ci not the dummy cj used for indexing)*/
     if(ci_start != cj_start){
-//      printf("Doing cj\n");
+      //      printf("Doing cj\n");
       /*We've done ci with cj, now interact cj with ci*/
       cell_starts_ends_read.x = cj_start;
       cell_starts_ends_read.y = cj_end;
@@ -142,15 +149,16 @@ __global__ void cuda_launch_density(
       cell_starts_ends_write.x = cj_write_start;
       cell_starts_ends_write.y = cj_write_end;
       //TODO: Un-necesary as we only need to write to cj but leave for now///
-//      cell_starts_ends_write.z = ci_start;
-//      cell_starts_ends_write.w = ci_end;
+      //      cell_starts_ends_write.z = ci_start;
+      //      cell_starts_ends_write.w = ci_end;
       const double3 shift_ii_res = {cj_loc.x.x, cj_loc.x.y, cj_loc.x.z};
       const double3 shift_jj_res = {shift_ix, shift_iy, shift_iz};
       ///////////////////////////////////////////////////////////////////////
       cuda_kernel_density(cid, d_parts_send, d_parts_recv, d_a, d_H,
-    		  cell_starts_ends_read, cell_starts_ends_write,
-    		  space_dim, shift_ii_res, shift_jj_res);
+          cell_starts_ends_read, cell_starts_ends_write,
+          space_dim, shift_ii_res, shift_jj_res);
     }
+  }
 //  }
 }
 
