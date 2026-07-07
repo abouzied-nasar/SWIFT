711a93421185384cdf87c1e62f9adb96d33010aa
Abouzied <abouzied.nasar@gmail.com>
2026-07-07 12:30:35 +0100

    Modified timers so we now use GPU timers when we do not offload. The timers seem to have been messed up so this is an initial stab at fixing them. Will need more work but fine for testing for now

--- src/runner_doiact_functions_hydro.h
+++ src/runner_doiact_functions_hydro.h
@@ -3055,8 +3055,11 @@ void DOSUB_PAIR1(struct runner *r, struct cell *ci, struct cell *cj,
       }
     }
   }
-
+#ifdef WITH_CUDA
+  if (gettimer) TIMER_TOC(timer_gpu_pack_d);
+#else
   if (gettimer) TIMER_TOC(TIMER_DOSUB_PAIR);
+#endif
 }
 
 /**
@@ -3118,8 +3121,11 @@ void DOSUB_SELF1(struct runner *r, struct cell *c, int recurse_below_h_max,
       }
     }
   }
-
+#ifdef WITH_CUDA
+  if (gettimer) TIMER_TOC(timer_gpu_pack_d);
+#else
   if (gettimer) TIMER_TOC(TIMER_DOSUB_SELF);
+#endif
 }
 
 /**
@@ -3223,8 +3229,11 @@ void DOSUB_PAIR2(struct runner *r, struct cell *ci, struct cell *cj,
       }
     }
   }
-
+#ifdef WITH_CUDA
+  if (gettimer) TIMER_TOC(timer_gpu_pack_d);
+#else
   if (gettimer) TIMER_TOC(TIMER_DOSUB_PAIR);
+#endif
 }
 
 /**
@@ -3284,8 +3293,11 @@ void DOSUB_SELF2(struct runner *r, struct cell *c, int recurse_below_h_max,
       }
     }
   }
-
+#ifdef WITH_CUDA
+  if (gettimer) TIMER_TOC(timer_gpu_pack_d);
+#else
   if (gettimer) TIMER_TOC(TIMER_DOSUB_SELF);
+#endif
 }
 
 /**
