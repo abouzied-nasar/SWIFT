eed3c9777c3546e38d97d6619f99959b037e931b
Abouzied <abouzied.nasar@gmail.com>
2026-07-07 12:02:51 +0100

    Added message to say whether we offload or not

--- src/runner_main_cuda.c
+++ src/runner_main_cuda.c
@@ -228,7 +228,7 @@ void *runner_main_cuda(void *data) {
      * Currently we use gpu_buf_dens to hold md->n_active_leaves.
      * Could use any of the other buffers*/
     const int offload = runner_GPU_offload_switch(r, sched, e, &gpu_buf_dens, /*timer off 0, on 1*/1);
-    if(offload)
+    if(offload && r->cpuid == 0)
       message("OFFLOADING");
 
     gpu_data_buffers_init_step(&gpu_buf_dens);
