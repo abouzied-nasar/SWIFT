a57a40aac4620cd079e23bbe42827d6f0614fba3
Abouzied Nasar <abouzied.nasar@gmail.com>
2026-07-10 15:41:19 +0100

    Added another barrier before we start the first time step to ensure the correct memory allocated is reported

--- src/runner_main_cuda.c
+++ src/runner_main_cuda.c
@@ -209,8 +209,8 @@ void *runner_main_cuda(void *data) {
   int step = 0;
 #endif
 
-  /*TODO: Should we put a barrier here to ensure we report the correct remaining memory?*/
-//  swift_barrier_wait(&e->wait_barrier);
+  /*TODO: Barrier here to ensure we report the correct remaining memory?*/
+  swift_barrier_wait(&(e->gpu_barrier));
   /* Tell me how much memory we're using. */
   gpu_print_free_mem(e, r->cpuid);
 
